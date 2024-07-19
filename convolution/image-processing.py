e5bd278684732adb02fab35eeacdb04253ad6c87
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2024-07-19 12:15:18 +0200

    edge detection works

---
+++ convolution/image-processing.py
@@ -0,0 +1,49 @@
+"""
+matsjfunke
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from scipy.ndimage import convolve
+
+
+def load_img(img_path):
+    # Load the image and convert to grayscale
+    img = Image.open(img_path).convert("L")
+
+    # Get image dimensions
+    width, height = img.size
+    print(f"Image dimensions: {width} x {height}")
+
+    # Convert image to numpy array
+    img_array = np.array(img)
+
+    return img_array
+
+
+def convolve_img(img_array, kernel):
+
+    convolved_img = convolve(img_array, kernel)  # Apply convolution
+
+    # Display original and convolved images
+    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+    axes[0].imshow(img_array, cmap="gray")
+    axes[0].set_title("Original Image")
+    axes[0].axis("off")
+
+    axes[1].imshow(convolved_img, cmap="gray")
+    axes[1].set_title("Convolved Image")
+    axes[1].axis("off")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    img_path = "images/pixel-man.png"
+    img_array = load_img(img_path)
+
+    # kernel for edge detection
+    edge_kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+
+    convolve_img(img_array, edge_kernel)
