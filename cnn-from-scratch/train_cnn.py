f4c6cbf6dfbe9a01f087710204aebbd3c78f8a72
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2024-07-28 06:49:31 +0200

    initalizing kernels and calculating featuremaps works

--- cnn-from-scratch/train_cnn.py
+++ cnn-from-scratch/train_cnn.py
@@ -2,7 +2,23 @@
 matsjfunke
 """
 
+import numpy as np
 from cifar_10_utils import load_cifar10, rgb2gray_weighted
+from scipy.signal import convolve2d
+
+
+def forward_pass(input_img_array, num_kernels, kernel_size):
+    # initalize kernels with random numbers
+    kernels = [np.random.randn(kernel_size, kernel_size) for _ in range(num_kernels)]
+
+    # convolve image with kernels to create feature_maps
+    feature_maps = []
+    for kernel in kernels:
+        feature_map = convolve2d(input_img_array, kernel, mode="valid")
+        feature_maps.append(feature_map)
+
+    return feature_maps
+
 
 if __name__ == "__main__":
     path = "../images/cifar-10-batches-py"
@@ -12,3 +28,6 @@ if __name__ == "__main__":
     # Convert to grayscale using weighted average
     train_images_gray = rgb2gray_weighted(train_images)
     test_images_gray = rgb2gray_weighted(test_images)
+
+    output = forward_pass(train_images_gray[0], 2, 3)
+    print(output)
